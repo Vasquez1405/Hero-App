d1868e60ddf890859144ded372088b10be5015ed
Oscar Vasquez <oscareduc14@hotmail.com>
2023-10-15 20:08:51 -0500

    primer commit

---
+++ src/heroes/pages/DcPage.jsx
@@ -0,0 +1,12 @@
+import { HeroList } from "../componentes"
+
+export const DcPage = () => {
+  return (
+    <>
+        <h1>Dc Comics</h1>
+        <hr />
+
+        <HeroList publisher='DC Comics'/>
+    </>
+  )
+}
